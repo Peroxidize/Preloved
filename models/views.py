e53f628397303d7e6aba2bf1e4af0b712e14b6e8
Jasper Keith O. Vallecera <jasperkeith.vallecera@cit.edu>
2024-10-07 21:22:15 +0800

    Fix get_similar_items

--- models/views.py
+++ models/views.py
@@ -9,10 +9,13 @@ from . import extractor
 
 def get_similar_items(request):
     itemID = request.GET.get('item_id')
+    num = request.GET.get('num')
     item = Item.objects.get(pk=itemID)
-    img = Slug.objects.filter(item=item).first()
-    items = query_database(extractor.extract_features(img), not_equal_to=img.itemID)
+    img = Slug.objects.filter(itemID=itemID).first()
+    items = query_database(extractor.extract_features(img), n=num, not_equal_to=img.itemID)
     return JsonResponse({"items" : items})
+
+
 
 
 
