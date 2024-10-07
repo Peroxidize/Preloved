1e4120d14ac844cd86a3eeb5addacb34d24d7cad
Jasper Keith O. Vallecera <jasperkeith.vallecera@cit.edu>
2024-10-07 21:51:18 +0800

    get_similar_items fix 2

--- models/views.py
+++ models/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from store.models import Slug, Item
 from .services import download_image, cascade_update_on_startup, query_database
 from . import extractor
+from homepage.views import HomePageController
 
 # cascade_update_on_startup()
 # Create your views here.
@@ -10,8 +11,11 @@ from . import extractor
 def get_similar_items(request):
     itemID = request.GET.get('item_id')
     num = request.GET.get('num')
-    img = Slug.objects.filter(itemID=itemID).first()
-    items = query_database(extractor.extract_features(img), n=num, not_equal_to=itemID)
+    slug = Slug.objects.filter(itemID=itemID).first()
+    imgURL = HomePageController.generate_link(slug.slug)
+    img = download_image(imgURL)
+    features = extractor.extract_features(img)
+    items = query_database(features, n=num, not_equal_to=itemID)
     return JsonResponse({"items" : items})
 
 
